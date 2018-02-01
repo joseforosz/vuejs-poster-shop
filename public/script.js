09fadb574214a101faca5299c1ad57c5aa41e7e3
Joe Orosz <joseforosz@gmail.com>
2018-02-01 18:53:56 -0500

    project1: video 24 adding price to cart

--- public/script.js
+++ public/script.js
@@ -1,30 +1,46 @@
 new Vue({
   el: '#app',
-  data:{
-    total:  0,
-    items:[
-      {title: 'Item 1'},
-      {title: 'Item 2'},
-      {title: 'Item 3'},
+  data: {
+    total: 0,
+    items: [{
+        id: 1,
+        title: 'Item 1',
+        price: 9.99
+      },
+      {
+        id: 2,
+        title: 'Item 2',
+        price: 5.99
+      },
+      {
+        id: 3,
+        title: 'Item 3',
+        price: 11.99
+      },
     ],
     cart: []
   },
-methods:{
-  addItem: function(index){
-    this.total += 10;
-    var item = this.items[index];
-    for(i=0; i<this.cart.length; i++){
-      if(this.cart[i]===item){
-        this.cart[i].qty++;
+  methods: {
+    addItem: function(index) {
+      var item = this.items[index];
+      this.total += item.price;
+      var found = false;
+      for (i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true
+          this.cart[i].qty++;
+        }
+      };
+
+      if (!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          qty: 1
+        })
       }
-    };
-    this.cart.push(
-      {
-        title: item.title,
-        qty: item.q
-      }
-      )
+    }
   }
-}
 
 })
