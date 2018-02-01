5d96d46c35f08fc94a984142b761648d7e5a0d12
Joe Orosz <joseforosz@gmail.com>
2018-02-01 17:59:36 -0500

    project1: been too long without a commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,30 @@
-console.log('It works.');
+new Vue({
+  el: '#app',
+  data:{
+    total:  0,
+    items:[
+      {title: 'Item 1'},
+      {title: 'Item 2'},
+      {title: 'Item 3'},
+    ],
+    cart: []
+  },
+methods:{
+  addItem: function(index){
+    this.total += 10;
+    var item = this.items[index];
+    for(i=0; i<this.cart.length; i++){
+      if(this.cart[i]===item){
+        this.cart[i].qty++;
+      }
+    };
+    this.cart.push(
+      {
+        title: item.title,
+        qty: item.q
+      }
+      )
+  }
+}
+
+})
